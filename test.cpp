5492172fa80636b825848d3e13edf5a58b5f9f59
Zzzzzzj <1019306750@qq.com>
2016-09-25 00:40:17 +0800

    new branch about control the number of out sentence

--- test.cpp
+++ test.cpp
@@ -2,20 +2,22 @@
 #include <map>
 #include <deque>
 #include <vector>
+#include <stdlib.h>     /* srand, rand */
 #include <string>
 #include <cstdio>
-#include <ctime>  
-#include <fstream>  
-#include <sstream>  
-#include <stdlib.h>  
+#include <sstream>
+#include <ctime>
+#include <fstream>
+
+
 using namespace std;
 
 enum {
      NPREF  =  2 ,           //前缀的个数
      MAXGEN  =  10000  //最多有那么多个词
-};  
+};
 
-typedef deque<string> Prefix; 
+typedef deque<string> Prefix;
 map<Prefix,vector<string> > statetab; // 使用duque 作为第一项方便前缀词数扩展和改变，这里statetab将前缀和后缀关联起来
 
 char  NONWORD[] = "\n"; //伪前缀，将真正的输入作为它的后缀
@@ -37,56 +39,73 @@ void build(Prefix &prefix, istream &in)//构造统计表
           add(prefix,buf);
 }
 
+bool IncludeCh(string str, char ch) {
+    int i;
+    bool has = false;
+    for(i = 0; str[i]; ++i) {
+        if(str[i] == ch) 
+            return true;
+    }
+    return false;
+}
+
 void generate(int nwords)
 {
      Prefix prefix;
      int i;
+     int j = 0;
      srand(time(NULL));
-	 string str1,str2;
-	 cout<<"print prefix 1";
-	 cin>>str1;
-     add(prefix,str1);//生成伪前缀
-	 cout<<"print prefix 2";
-	 cin>>str2;
-     add(prefix,str2);//生成伪前缀
-     //cout<<"NOW prefix: "<<prefix[0]<<" "<<prefix[1]<<endl;
+     string str1, str2;
+     cout << "please input prefix 1: " ;
+     cin >> str1;
+     add(prefix, str1);
+     cout << "please input prefix 2: " ;
+     cin >> str2;
+     add(prefix, str2);
+     cout << str1 << " " << str2 << " ";
+
      for(i = 0; i < nwords; i++)
      {
           vector<string> & suf = statetab[prefix];
           //cout<<"prefix:"<<prefix[0]<<" suf "<<suf[0]<<endl;
           const string &w = suf[rand() % suf.size()];//在所有可能的后缀中随机的选择一个词
-          if(w == str1 || w == str2)
+          if(w == NONWORD)
                break;
-          cout << w <<" ";
+			cout << w <<" ";
+		  if(IncludeCh(w,'.'))  
+		  { 
+			  j++;
+			  cout<<"j:"<<j<<endl;
+		  }
+		  if(j>=5) break;
           prefix.pop_front();//改变前缀
           prefix.push_back(w);
      }
+
      cout<<endl;
 }
 
-string readFileIntoString(char * filename)  
-{  
-ifstream ifile(filename);  
-//将文件读入到ostringstream对象buf中  
-ostringstream buf;  
-char ch;  
-while(buf&&ifile.get(ch))  
-buf.put(ch);  
-//返回与流对象buf关联的字符串  
-return buf.str();  
-}  
-  
+string readFileInfoString(char * filename)
+{
+    ifstream ifile(filename);
+    ostringstream buf;
+    char ch;
+    while(buf&&ifile.get(ch))
+    buf.put(ch);  //返回与流对象buf关联的字符串
+    return buf.str();
+}
 
 int main()
 {
      int nwords = MAXGEN;
      Prefix prefix;
-	 char * fn = "example.txt";
-	 string str;
-	 str = readFileIntoString(fn);
-	 cout<<"read:"<<str<<endl;
-     //string sentence = "Show your flowchars and conceal your tables and I will be mystified. Show your tables and your flowcharts will be obvious. (end)";
-     istringstream in(str);
+    char * fn = "example.txt";
+    string str;
+    str = readFileInfoString(fn);
+//    cout << "read: " << str << endl;
+
+//     string sentence = "Show your flowchars and conceal your tables and I will be mystified. Show your tables and your flowcharts will be obvious. (end)";
+     istringstream in(str); //C++风格的串流的输入操作。
      //freopen("mkov.txt","r",stdin);
      for(int i = 0; i < NPREF; i++)
           add(prefix,NONWORD);
